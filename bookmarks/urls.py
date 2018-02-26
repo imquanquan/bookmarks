220e976597d25965c93cd42203c5044fdec76d90
imquanquan <imquanquan99@gmail.com>
2018-02-26 14:49:50 +0800

    :art:add favicon:relieved:

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,7 +18,10 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('books.urls')),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/media/favicon.ico')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
