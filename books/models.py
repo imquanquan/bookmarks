695d0b12c5dd021cd88200e4fb3245c2caabab4c
imquanquan <imquanquan99@gmail.com>
2018-03-06 19:58:12 +0800

    :art:change category long:relieved:

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     '''
     Books categories
     '''
-    name = models.CharField(max_length = 10)
+    name = models.CharField(max_length = 25)
 
     def __str__(self):
         return self.name
