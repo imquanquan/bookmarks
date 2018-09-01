0dad8f23710033107f5030d6efbc152bcb33f71d
imquanquan <imquanquan99@gmail.com>
2018-09-01 11:39:31 +0800

    [add]add Mariadb driver

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -72,13 +72,23 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'bookmarks',
+    'USER':'book',
+    'PASSWORD': 'book',
+    'HOST':'localhost',
+    'PORT':'',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.generic.base import RedirectView
 
@@ -24,4 +24,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('books.urls')),
     url(r'^favicon\.ico$', RedirectView.as_view(url='/media/favicon.ico')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + staticfiles_urlpatterns()
